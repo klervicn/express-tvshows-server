4a7518c2490f6e709387ec10d461f8b2f77ebc75
klervicn <corbel.klervi@gmail.com>
2017-02-27 13:17:35 +0100

    post v2

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var uuid = require('uuid4')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 var series = []
+var seasons = []
 
-series.push({name: 'Dr Who', id: '1', path: 'C:\Users\Klervi\Pictures\tardis.png', nbSeasons: 9})
-series.push({name: 'Pokemon', id: '2', path: 'C:\Users\Klervi\Pictures\slowpoke.png', nbSeasons: 12})
-series.push({name: 'Orange is the new black', id: '3', path: 'C:\Users\Klervi\Pictures\orange_is_the_new_black.png', nbSeasons: 4})
+series.push({name: 'Dr Who', id: '1', path: 'C:\Users\Klervi\Pictures\tardis.png'})
+series.push({name: 'Pokemon', id: '2', path: 'C:\Users\Klervi\Pictures\slowpoke.png'})
+series.push({name: 'Orange is the new black', id: '3', path: 'C:\Users\Klervi\Pictures\orange_is_the_new_black.png'})
+seasons.push({name: 'Saison 1', idSerie: '1', id : String(uuid())})
+seasons.push({name: 'Saison 2', idSerie: '1', id : String(uuid())})
+seasons.push({name: 'Saison 3', idSerie: '1', id : String(uuid())})
+seasons.push({name: 'Saison 1', idSerie: '2', id : String(uuid())})
+seasons.push({name: 'Saison 1', idSerie: '3', id : String(uuid())})
+seasons.push({name: 'Saison 2', idSerie: '3', id : String(uuid())})
 
-app.post('/addSerie', function (req, res, next) {
+app.post('/', function (req, res, next) {
   var name = req.body.name
-  var id = req.body.id
+  var id = String(uuid())
   var path = req.body.path
-  var nbSeasons = req.body.nbSeasons
 
-  series.push({name, id, path, nbSeasons})
-  res.send(name + ' ' + id + ' ' + nbSeasons)
+  series.push({name, id, path})
+  res.send(name + ' ' + id)
   next()
 })
+
+app.post('/:tvShowId', function (req, res, next) {
+  var name = req.body.name
+  var idSerie = req.params.tvShowId
+  var id = String(uuid())
+
+  seasons.push({name, idSerie, id})
+  res.send('Nouvelle saison ajoutée ' + id)
+  next()
+})
+
 
 // GET/ "retourne un objet contenant la liste des séries"
 app.get('/', function (req, res, next) {
@@ -36,10 +54,17 @@ app.get('/', function (req, res, next) {
 app.get('/:tvShowId', function (req, res, next) {
   res.setHeader('Content-Type', 'application/json')
   var selectedSerie = series.find(function (serie) {
-    return serie.id === req.params.tvShowId
+    return String(serie.id) === req.params.tvShowId
   })
+  var seasonsArray = []
+
+  for (season of seasons){
+    if (season.idSerie === req.params.tvShowId ){
+      seasonsArray.push(season.name)
+    }
+  }
   res.write(JSON.stringify(selectedSerie))
-  res.write('Serie ' + req.params.tvShowId + ' : ' + JSON.stringify(selectedSerie.name))
+  res.write('Serie ' + req.params.tvShowId + ' : ' + JSON.stringify(selectedSerie.name) + ' Saisons : ' + JSON.stringify({seasonsArray}))
   res.end()
   next()
 })
