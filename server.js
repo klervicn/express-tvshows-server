d5a90c4e7facde7bb73984c075a18cabc6c916fa
klervicn <corbel.klervi@gmail.com>
2017-03-06 17:42:30 +0100

    array -> objects

--- server.js
+++ server.js
@@ -5,25 +5,27 @@ const BODY_PARSER = require('body-parser')
 const UUID = require('uuid4')
 APP.use(BODY_PARSER.json())
 APP.use(BODY_PARSER.urlencoded({ extended: true}))
-const SERIES = []
-const SEASONS = []
 
-SERIES.push({name: 'Dr Who', id: '1', path: 'C:\Users\Klervi\Pictures\tardis.png'})
-SERIES.push({name: 'Pokemon', id: '2', path: 'C:\Users\Klervi\Pictures\slowpoke.png'})
-SERIES.push({name: 'Orange is the new black', id: '3', path: 'C:\Users\Klervi\Pictures\orange_is_the_new_black.png'})
-SEASONS.push({name: 'Saison 1', idSerie: '1', id : String(UUID())})
-SEASONS.push({name: 'Saison 2', idSerie: '1', id : String(UUID())})
-SEASONS.push({name: 'Saison 3', idSerie: '1', id : String(UUID())})
-SEASONS.push({name: 'Saison 1', idSerie: '2', id : String(UUID())})
-SEASONS.push({name: 'Saison 1', idSerie: '3', id : String(UUID())})
-SEASONS.push({name: 'Saison 2', idSerie: '3', id : String(UUID())})
+const SERIES = {}
+const SEASONS = {}
+
+SEASONS[String(UUID())] = {name: 'Saison 1', idSerie: '1'}
+SEASONS[String(UUID())] = {name: 'Saison 2', idSerie: '1'}
+SEASONS[String(UUID())] = {name: 'Saison 3', idSerie: '1'}
+SEASONS[String(UUID())] = {name: 'Saison 1', idSerie: '2'}
+SEASONS[String(UUID())] = {name: 'Saison 1', idSerie: '3'}
+SEASONS[String(UUID())] = {name: 'Saison 2', idSerie: '3'}
+
+SERIES[1] = {name: 'Dr Who', url: 'http://www.hbc333.com/data/out/71/46987055-dr-who-wallpaper.jpg'}
+SERIES[2] = {name: 'Pokemon', url: 'http://cdn.bulbagarden.net/upload/thumb/7/70/079Slowpoke.png/250px-079Slowpoke.png'}
+SERIES[3] = {name: 'Orange is the new black', url: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Orange_is_the_new_Black.png'}
 
 APP.post('/', (req, res, next) => {
   var name = req.body.name
   var id = String(UUID())
-  var path = req.body.path
+  var url = req.body.url
 
-  SERIES.push({name, id, path})
+  SERIES[id] = {name, url}
   res.send(name + ' ' + id)
   next()
 })
@@ -33,7 +35,7 @@ APP.post('/:tvShowId', (req, res, next) => {
   var idSerie = req.params.tvShowId
   var id = String(UUID())
 
-  SEASONS.push({name, idSerie, id})
+  SEASONS[id] = {name, idSerie}
   res.send('Nouvelle saison ajoutée ' + id)
   next()
 })
@@ -42,15 +44,15 @@ APP.post('/:tvShowId', (req, res, next) => {
 // GET/ "retourne un objet contenant la liste des séries"
 APP.get('/', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
-  var seriesList = []
+/*var seriesList = []
   for (serie of SERIES) {
     seriesList.push(serie.name)
-  }
-  res.write(JSON.stringify({ seriesList }))
+  }*/
+  res.write(JSON.stringify({SERIES}))
   res.end()
   next()
 })
-
+/*
 // GET /<tvShowId>: retourne un objet contenant le titre de la série, l'URL de son image et la liste des saisons
 APP.get('/:tvShowId', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
@@ -74,6 +76,6 @@ APP.use( (req, res, next) => {
   res.setHeader('Content-Type', 'text/plain')
   res.send(404, 'Page introuvable !')
   next()
-})
+})*/
 
 APP.listen(8080)
