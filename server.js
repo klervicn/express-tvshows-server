8c8b67d0d9a1f2cb020f6973e9e859238ed8f5a8
klervicn <corbel.klervi@gmail.com>
2017-04-01 16:43:08 +0200

    update post

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const uuid = require('uuid4')
+const slugify = require('slugify')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
 const series = {}
-const seasons = {}
 
-seasons[String(uuid())] = {name: 'Saison 1', idSerie: '1'}
-seasons[String(uuid())] = {name: 'Saison 2', idSerie: '1'}
-seasons[String(uuid())] = {name: 'Saison 3', idSerie: '1'}
-seasons[String(uuid())] = {name: 'Saison 1', idSerie: '2'}
-seasons[String(uuid())] = {name: 'Saison 1', idSerie: '3'}
-seasons[String(uuid())] = {name: 'Saison 2', idSerie: '3'}
+series['Dr_Who'] = {url: 'http://www.hbc333.com/data/out/71/46987055-dr-who-wallpaper.jpg', seasons: []}
+series['Pokemon'] = {url: 'http://cdn.bulbagarden.net/upload/thumb/7/70/079Slowpoke.png/250px-079Slowpoke.png', seasons: []}
+series['Orange_is_the_new_Black'] = {url: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Orange_is_the_new_Black.png', seasons: []}
 
-series[1] = {name: 'Dr Who', url: 'http://www.hbc333.com/data/out/71/46987055-dr-who-wallpaper.jpg'}
-series[2] = {name: 'Pokemon', url: 'http://cdn.bulbagarden.net/upload/thumb/7/70/079Slowpoke.png/250px-079Slowpoke.png'}
-series[3] = {name: 'Orange is the new black', url: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Orange_is_the_new_Black.png'}
+function isSeriesExisting (seriesName) {
+  if (series[seriesName] === undefined) return false
+  else return true
+}
 
-// POST /: insère une nouvelle série et retourne son identifant généré par le serveur
 app.post('/', (req, res, next) => {
-  const name = req.body.name
-  const id = String(uuid())
+  const name = slugify(req.body.name, '_')
   const url = req.body.url
+  const seasons = []
 
-  series[id] = {name, url}
-  res.send(name + ' ' + id)
-  next()
+  if (!isSeriesExisting(name)) {
+    series[name] = {url, seasons}
+    res.json(name)
+    next()
+  } else next()
 })
 
-// POST /: insère une nouvelle saison et retourne son identifant généré par le serveur
-app.post('/:tvShowId', (req, res, next) => {
-  const name = req.body.name
-  const idSerie = req.params.tvShowId
-  const id = String(uuid())
+app.post('/:tvShowName', (req, res, next) => {
+  const seriesName = req.params.tvShowName
+  const seasonName = slugify(req.body.name) // '-'
 
-  seasons[id] = {name, idSerie}
-  res.send('Nouvelle saison ajoutée ' + id)
-  next()
+  if (isSeriesExisting(seriesName)) {
+    if (series[seriesName].seasons.indexOf(seasonName) === -1) {
+      series[seriesName].seasons.push(seasonName)
+      res.json(series[seriesName].seasons)
+      next()
+    } else next()
+  } else next()
 })
 
-// GET/ "retourne un objet contenant la liste des séries"
 app.get('/', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
   res.write(JSON.stringify(series))
@@ -49,21 +49,23 @@ app.get('/', (req, res, next) => {
   next()
 })
 
-// GET /<tvShowId>: retourne un objet contenant le titre de la série, l'URL de son image et la liste des saisons
-app.get('/:tvShowId', (req, res, next) => {
+app.get('/:tvShowName', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
-  const selectedSerie = series[req.params.tvShowId]
-  const seasonsArray = []
+  const selectedSerie = series[req.params.tvShowName]
 
-  for (id in seasons) {
-    if (seasons[id].idSerie === req.params.tvShowId) {
-      seasonsArray.push(seasons[id].name)
-    }
-  }
-
-  res.write('Serie ' + req.params.tvShowId + ' : ' + JSON.stringify(selectedSerie.name) + ' Saisons : ' + JSON.stringify(seasonsArray))
+  res.write('Serie ' + req.params.tvShowName + ' : ' + ' Saisons : ' + JSON.stringify(selectedSerie.seasons))
   res.end()
   next()
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Not found !')
+})
+
+app.use(function (req, res, next) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.send(404, 'Not found !')
 })
 
 app.listen(8080)
